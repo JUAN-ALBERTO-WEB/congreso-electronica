de97a06169bb6b9bc8041340f7902176ff73b9b2
JUAN-ALBERTO-WEB <alexepiet@gmail.com>
2025-09-12 21:05:40 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,18 +2,28 @@
 const contador = document.getElementById("contador");
 const fechaEvento = new Date("Oct 2, 2025 09:00:00").getTime();
 
-setInterval(() => {
+function actualizarContador() {
   const ahora = new Date().getTime();
-  const distancia = fechaEvento - ahora;
+  const diferencia = fechaEvento - ahora;
 
-  const dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
-  const horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
-  const segundos = Math.floor((distancia % (1000 * 60)) / 1000);
+;
 
-  contador.innerHTML = `${dias}d ${horas}h ${minutos}m ${segundos}s`;
-
-  if (distancia < 0) {
-    contador.innerHTML = "¡El evento ha comenzado!";
+  if (diferencia < 0) {
+    contador.innerHTML = "¡El Congreso ha comenzado!";
+    return;
   }
-}, 1000);
+
+  const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+  const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+  const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+
+  contador.innerHTML = `
+    <div class="time-box"><span>${dias}</span>Días</div>
+    <div class="time-box"><span>${horas}</span>Horas</div>
+    <div class="time-box"><span>${minutos}</span>Min</div>
+    <div class="time-box"><span>${segundos}</span>Seg</div>
+  `;
+}
+
+setInterval(actualizarContador, 1000);
